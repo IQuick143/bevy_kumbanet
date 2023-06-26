9383bfcfc9bb0dba106fbf20de510f690a36db3c
IQuick143 <iquick143cz@gmail.com>
2023-06-26 21:03:56 +0200

    added cabin thought dynamics

--- src/cabin/mod.rs
+++ src/cabin/mod.rs
@@ -1,6 +1,9 @@
-use bevy::{prelude::*, render::{view::RenderLayers, camera::{RenderTarget, ScalingMode}}, core_pipeline::clear_color::ClearColorConfig};
+use bevy::{prelude::*, render::{view::RenderLayers, camera::{RenderTarget, ScalingMode}}, core_pipeline::clear_color::ClearColorConfig, math::Vec3Swizzles};
 
 use crate::prelude::*;
+
+pub const CABIN_WIDTH: f32 = 16.0;
+pub const CABIN_HEIGHT: f32 = 9.0;
 
 pub struct CabinPlugin;
 
@@ -16,6 +19,7 @@ impl Plugin for CabinPlugin {
 			update_cursor_position,
 			track_cursor,
 			spawn_collected_thoughts,
+			move_cabin_thoughts,
 		).chain())
 		;
 	}
@@ -32,7 +36,7 @@ fn spawn_cabin_camera(
 				target: RenderTarget::Image(render_target.texture.clone()),
 				order: 10, is_active: true, ..Default::default()
 			},
-			projection: OrthographicProjection {scaling_mode: ScalingMode::Fixed{width: 16.0, height: 9.0}, ..Default::default()},
+			projection: OrthographicProjection {scaling_mode: ScalingMode::Fixed{width: CABIN_WIDTH, height: CABIN_HEIGHT}, ..Default::default()},
 			camera_2d: Camera2d {clear_color: ClearColorConfig::None},
 			..Default::default()
 		},
@@ -48,7 +52,7 @@ fn spawn_cabin(
 ) {
 	commands.spawn((
 		SpriteBundle {
-			sprite: Sprite {custom_size: Some(Vec2::new(16.0, 9.0)), ..Default::default()},
+			sprite: Sprite {custom_size: Some(Vec2::new(CABIN_WIDTH, CABIN_HEIGHT)), ..Default::default()},
 			texture: asset_server.load("ui/hud.png"),
 			transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
 			..Default::default()
@@ -94,8 +98,43 @@ fn spawn_cabin_thought(
 		},
 		RenderLayers::layer(1),
 		Name::new(format!("CabinThought {:?}", thought.word)),
+		Velocity(Vec3::new(1.0,1.0,0.0)),
 		CabinThought(thought),
 	));
+}
+
+fn move_cabin_thoughts(
+	mut cabin_thought_query: Query<(&mut Transform, &mut Velocity), With<CabinThought>>,
+	mouse_pos: Res<CursorCabinPosition>,
+	inputs: Res<Input<MouseButton>>,
+	time: Res<Time>
+) {
+	for (mut transform, mut velocity) in cabin_thought_query.iter_mut() {
+		if transform.translation.x < -CABIN_WIDTH / 2.0 && velocity.x < 0.0 {
+			transform.translation = Vec3::new(-CABIN_WIDTH / 2.0, transform.translation.y, transform.translation.z);
+			velocity.0 = Vec3::new(-velocity.x, velocity.y, velocity.z);
+		} else if transform.translation.x > CABIN_WIDTH / 2.0 && velocity.x > 0.0 {
+			transform.translation = Vec3::new(CABIN_WIDTH / 2.0, transform.translation.y, transform.translation.z);
+			velocity.0 = Vec3::new(-velocity.x, velocity.y, velocity.z);
+		}
+		if transform.translation.y < -CABIN_HEIGHT / 2.0 && velocity.y < 0.0 {
+			transform.translation = Vec3::new(transform.translation.x, -CABIN_HEIGHT / 2.0, transform.translation.z);
+			velocity.0 = Vec3::new(velocity.x, -velocity.y, velocity.z);
+		} else if transform.translation.y > CABIN_HEIGHT / 2.0 && velocity.y > 0.0 {
+			transform.translation = Vec3::new(transform.translation.x, CABIN_HEIGHT / 2.0, transform.translation.z);
+			velocity.0 = Vec3::new(velocity.x, -velocity.y, velocity.z);
+		}
+	}
+
+	if inputs.any_pressed([MouseButton::Left, MouseButton::Middle, MouseButton::Right]) {
+		for (transform, mut velocity) in cabin_thought_query.iter_mut() {
+			let r = (transform.translation.xy() - mouse_pos.world_position).extend(0.0);
+			let r_square = r.length_squared();
+			if r_square > 0.01 {
+				velocity.0 += -(time.delta_seconds() * 3.0 / r_square) * r.normalize()
+			}
+		}
+	}
 }
 
 fn track_cursor(
@@ -118,8 +157,8 @@ fn update_cursor_position(
 			pos.y / window.height(),
 		);
 		cursor.world_position = Vec2::new(
-			(cursor.uv_position.x - 0.5) * 16.0,
-			(cursor.uv_position.y - 0.5) *  9.0,
+			(cursor.uv_position.x - 0.5) * CABIN_WIDTH,
+			(cursor.uv_position.y - 0.5) *  CABIN_HEIGHT,
 		);
 	}
 }
