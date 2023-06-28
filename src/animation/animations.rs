0866bd8d4400de2e3a897c1bee45a0f49e25a61b
IQuick143 <iquick143cz@gmail.com>
2023-06-28 15:28:29 +0200

    add some animation paths

--- src/animation/animations.rs
+++ src/animation/animations.rs
@@ -10,12 +10,35 @@ impl AnimationPath for Stationary {
 }
 
 #[derive(Clone, Copy, Debug)]
-pub struct Sine {
-	pub direction: Vec3,
-	pub angular_frequency: f32,
+pub struct Ellipse {
+	pub major_semiaxis: Vec3,
+	pub minor_semiaxis: Vec3,
+	pub frequency: f32,
 }
-impl AnimationPath for Sine {
+impl Ellipse {
+	// Makes an ellipse that is a circle in the X-Y plane
+	pub fn circle(radius: f32, frequency: f32) -> Self {
+		Ellipse {
+			major_semiaxis: Vec3::new(radius, 0.0, 0.0),
+			minor_semiaxis: Vec3::new(0.0, radius, 0.0),
+			frequency,
+		}
+	}
+}
+impl AnimationPath for Ellipse {
 	fn get_point(&self, t: f32) -> bevy::prelude::Vec3 {
-		return f32::sin(self.angular_frequency * t) * self.direction;
+		let p = 2.0 * std::f32::consts::PI * self.frequency * t;
+		return f32::cos(p) * self.major_semiaxis + f32::sin(p) * self.minor_semiaxis;
+	}
+}
+
+#[derive(Clone, Debug)]
+pub struct Sum {
+	a: Box<dyn AnimationPath>,
+	b: Box<dyn AnimationPath>,
+}
+impl AnimationPath for Sum {
+	fn get_point(&self, t: f32) -> bevy::prelude::Vec3 {
+		self.a.get_point(t) + self.b.get_point(t)
 	}
 }
